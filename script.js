34f7ffb768d5b83a05752d815e32cae8bfd8dde4
Maxence <kiger.maxence@gmail.com>
2019-01-04 19:49:13 +0100

    end

---
+++ script.js
@@ -0,0 +1,3 @@
+
+setTimeout(function maFonction() {if (confirm("veux-tu rentrer sur xXxVidsxXx (svp je dois faire mes thunes)")) { window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ");} else {}
+},10000);
